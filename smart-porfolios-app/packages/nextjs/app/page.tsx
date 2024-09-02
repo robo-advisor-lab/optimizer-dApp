11b8ed562bea5d2703ddef6f0df5a14967f4f8e8
Antonio Tellez <antoniotellez52@gmail.com>
2024-09-01 18:53:58 -0600

    adding page for products and main information

--- smart-porfolios-app/packages/nextjs/app/page.tsx
+++ smart-porfolios-app/packages/nextjs/app/page.tsx
@@ -11,56 +11,73 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
+      <div className="grid grid-cols-2 gap-4 pt-10">
+        <div className="px-5 items-center mt-40">
+          <div>
+            <h1 className="text-center">
+              <span className="block text-2xl mb-2">Welcome to</span>
+              <span className="block text-4xl font-bold">SMART PORTFOLIOS</span>
+            </h1>
+            <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
+              <p className="my-2 font-medium">Connected Address:</p>
+              <Address address={connectedAddress} />
+            </div>
+            <p className="text-center  block text-2xl font-bold">
+              <h1> "Explore Data-Driven DeFi Investing" </h1>
+            </p>
+            <p className="text-center text-lg">
+              Edit your smart contract{" "}
+              <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+                YourContract.sol
+              </code>{" "}
+              in{" "}
+              <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+                packages/hardhat/contracts
+              </code>
+            </p>
           </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
         </div>
+        <div className="px-5">
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img src="/i-4.jpeg" alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">
+                Make the leap to Web3. Invest in BTC/ETH and discover the future of decentralized finance!
+              </h2>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
+              <div className="card-actions">
+                <a href="/products">
+                  <button className="btn btn-primary">Buy Now</button>
+                </a>
+              </div>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12 ">
+        <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+          <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <BugAntIcon className="h-8 w-8 fill-secondary" />
+            <p>
+              Tinker with your smart contract using the{" "}
+              <Link href="/debug" passHref className="link">
+                Debug Contracts
+              </Link>{" "}
+              tab.
+            </p>
+          </div>
+
+          <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+            <p>
+              Explore your local transactions with the{" "}
+              <Link href="/blockexplorer" passHref className="link">
+                Block Explorer
+              </Link>{" "}
+              tab.
+            </p>
           </div>
         </div>
       </div>
---
+++ smart-porfolios-app/packages/nextjs/app/products/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src="/i-5.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Optimize Your Portfolio Today</h1>
+            <p className="py-6">"Join the DeFi Revolution"</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="p-11">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-28">
+          {" "}
+          {/* Ajusta la grilla para dispositivos móviles */}
+          <div className="card bg-gray-500 w-full md:w-96 shadow-xl items-center">
+            {" "}
+            {/* Ajusta el ancho en pantallas pequeñas */}
+            <figure className="px-10 pt-10"></figure>
+            <div className="card-body items-center text-center">
+              <a href="/">
+                <button className="btn btn-lg mt-10 btn-glass mb-5">BTC/ETH</button>
+              </a>
+            </div>
+          </div>
+          <div className="card bg-gray-500 w-full md:w-96 shadow-xl">
+            {" "}
+            {/* Ajusta el ancho en pantallas pequeñas */}
+            <figure className="px-10 pt-10"></figure>
+            <div className="card-body items-center text-center">
+              <a href="/">
+                <button className="btn btn-lg mt-10 bg-glass btn-rounded mb-5">
+                  <h2>RWA ETF on-chain</h2> {/* Considera usar un tamaño de texto más adecuado para móviles */}
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
