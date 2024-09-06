f17155c28ad69b42baa1e2b7adced527ac7fbfa6
D9J9V <jimenezvdiego@gmail.com>
2024-09-06 10:49:00 -0600

    Deleting RWA. Will re-implement in the future

--- PLP/__init__.py
+++
@@ -1,2 +0,0 @@
-from .utils import preprocess, simulation, optimization, visualization, risk_management
-from .private_loans_portfolio import main
--- PLP/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .preprocess import load_data, preprocess_data
-from .optimization import optimize_portfolio
-from .risk_management import calculate_greeks, var_calculation
-from .simulation import simulate_loan, backtest_strategy
-from .visualization import plot_efficient_frontier
