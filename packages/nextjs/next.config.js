cab921f138e79d324e84e62e82ee34940625198a
D9J9V <jimenezvdiego@gmail.com>
2024-09-07 09:39:00 -0600

    Ignoring wsl artifacts on build

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -22,6 +22,15 @@ const nextConfig = {
       },
     ];
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+      ],
+    },
+  },
 };
 
 module.exports = nextConfig;
